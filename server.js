1c1c932490efee30be8443a23c2a2edff25081eb
Muhammad Iezwan <anakc216@gmail.com>
2026-08-06 12:47:12 +0800

    Move frontend to public and secure Express static files

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false, limit: '1mb' }));
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const {
   RAZORPAY_KEY_ID,
@@ -144,7 +144,7 @@ app.get('/health', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.use((err, req, res, next) => {
